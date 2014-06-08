c857b94670d032fe3827cbada6ad89a5e915b769
Bjartnes <bjartnes@gmail.com>
2014-06-08 17:46:10 +0000

    statemachine

--- server.js
+++ server.js
@@ -20,32 +20,30 @@ function send(msg) {
    if (sending) throw "Can not send in sending state";
    sending = true;
    sp.write(msg, function(err, results) {
-   if (err) {
-       console.log('err ' + err);
-   } else {
-	sp.drain( function () { sending = false;});
-   }
- });
+	   if (err) {
+	       console.log('err ' + err);
+	   } else {
+		sp.drain( function () { sending = false;});
+	   }
+   });
 }
 
 function fsm() {
    var READY_TO_PROCESS_MSG = "ReadyToProcessMessage",
        INIT = "Init",
        PROCESSING_MSG = "ProcessingMessage",
-       state = INIT, 
-       messages = [];
-   function loop() {
-       if (state == INIT) {
-           if (serialOpen && !sending) {
-              state = READY_TO_PROCESS_MSG; 
-	   }
-       } else if (state == PROCESSING_MSG) {
-           if(!sending) {
-              state = READY_TO_PROCESS_MSG; 
-           }
-       } else if (state == READY_TO_PROCESS_MSG) {
-           if (messages.length > 0) {
-             state = PROCESSING_MSG; 
+       messages = [],
+       states = [INIT, READY_TO_PROCESS_MSG, PROCESSING_MSG],
+       state = INIT,
+       transitions = {},
+       actions = {};
+   transitions[INIT] = {};
+   transitions[INIT][READY_TO_PROCESS_MSG] = function () { return serialOpen && !sending;};
+   transitions[PROCESSING_MSG] = {};
+   transitions[PROCESSING_MSG][READY_TO_PROCESS_MSG] = function () {return !sending;};
+   transitions[READY_TO_PROCESS_MSG] = {};
+   transitions[READY_TO_PROCESS_MSG][PROCESSING_MSG] = function () {return messages.length > 0; };	
+   actions[PROCESSING_MSG] = function () {
 	     console.log("Processing " + messages);
              var msg = messages.pop(); 
              if (msg == 'red' || msg == 'green' || msg == 'blue' ) {
@@ -53,9 +51,16 @@ function fsm() {
                if (msg == 'green') send(color("000","255","000")); 
                if (msg == 'blue') send(color("000","000","255")); 
               }
-           }
-      }
-      setImmediate(loop); 
+   	     } 
+   function loop() {
+        for (possibleNewState in transitions[state]) {
+		if (transitions[state][possibleNewState]()) {
+			   state = possibleNewState;
+			   if (actions[state]) actions[state]();
+			   break;
+		}
+	}
+        setImmediate(loop); 
    }
    setImmediate(loop); 
    return function(msg) {
