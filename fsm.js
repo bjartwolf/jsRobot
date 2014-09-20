deeaf4ae3d098c0bf7f73139df000f2e8e29206d
Bjartnes <bjartnes@gmail.com>
2014-09-20 11:48:53 +0000

    drawing state machine

--- fsm.js
+++ fsm.js
@@ -1,17 +1,51 @@
-module.exports = function (transitions, actions, state) { 
-	function loop() {
-		for (possibleNewState in transitions[state]) {
-			if (transitions[state][possibleNewState]()) {
-				   // Changing state
-				   console.log("Changed state from " + state + " to " + possibleNewState); 
-				   state = possibleNewState;
-				   // If any actions are defined on the new state
-				   // activate the action
-				   if (actions[state]) actions[state]();
-				   break;
-			}
-		}
-		setImmediate(loop); 
-	   }
-	   loop(); 
+var graphviz = require('graphviz');
+
+var _transitions,
+    _actions,
+    _state,
+    _states;
+
+exports.machine = function (transitions, actions, state, states) { 
+  console.log("Booting FSM");
+  _transitions = transitions;
+  _actions = actions;
+  _state = state;
+  _states = states;
+  function loop() {
+    for (possibleNewState in _transitions[_state]) {
+      if (_transitions[_state][possibleNewState]()) {
+        // Changing state
+        console.log("Changed state from " + _state + " to " + possibleNewState); 
+        _state = possibleNewState;
+        // If any actions are defined on the new state
+        // activate the action
+        if (_actions[_state]) _actions[_state]();
+        // can draw here and send refresh signal to browser to get new image by websockets.
+        // draw(); 
+        break; // Only go to one state
+      }
+    }
+    setImmediate(loop); 
+  }
+  loop(); 
+}
+
+// Takes a http response stream and writes 
+// a png of current state machine to it
+exports.draw = function (res) {
+  var g = graphviz.digraph("G");
+  for (state in _states) {
+    var stateNode = g.addNode(_states[state], {"color" : "blue" });
+    if (_states[state] === _state) {
+      stateNode.set("style", "filled");
+    }
+    for (toState in _states) {
+      if (_transitions[_states[state]][_states[toState]]) {
+        g.addEdge(_states[state],_states[toState]);
+      }
+    }
+  }
+  g.render("png", function(render) {
+      res.end(render);
+  });
 }
--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 var http = require("http"),
-    rapiro = require("./rapiro.js");
+    rapiro = require("./rapiro.js"),
+    Sound = require("simple-mplayer"),
+    fsm = require('./fsm.js');
 
-function fsm() {
+function process_messages_fsm() {
+   //var hatexml = "./hatexml.mp3"; 
+   //var hatesound = new Sound(hatexml);
    var READY_TO_PROCESS_MSG = "ReadyToProcessMessage",
        INIT = "Init",
        PROCESSING_MSG = "ProcessingMessage",
@@ -17,27 +21,35 @@ function fsm() {
    transitions[READY_TO_PROCESS_MSG] = {};
    transitions[READY_TO_PROCESS_MSG][PROCESSING_MSG] = function () {return messages.length > 0; };	
    actions[PROCESSING_MSG] = function () {
+    //   hatesound.play();
 		     rapiro.send(messages.pop());
-   		} 
-   require("./fsm.js")(transitions, actions, state);
+   } 
+   fsm.machine(transitions, actions, state, states);
    return function(msg) {
       messages.unshift(msg);
    }
 }
 
-var queue = fsm();
-
+var queue = process_messages_fsm();
 http.createServer(function(req, res) {
-  var url = req.url.substring(1); // remove slash
-  if (url == 'red' || url == 'green' || url == 'blue') {
-     console.log("Queued " + url);
-     queue(url); 
+  var cmd = req.url.substring(1); // remove slash
+  if (cmd == 'red' || cmd == 'green' || cmd == 'blue') {
+     console.log("Queued " + cmd);
+     queue(cmd); 
      res.writeHead(200, {'Content-Type': 'text/plain'});
-     res.end(url + ' queued');
+     res.end(cmd + ' queued');
+ } else if (cmd == "pic") {
+     res.writeHead(200, {'Content-Type': 'image/png'});
+     fsm.draw(res);
+  } else if (cmd == "json") {
+     res.writeHead(200, {'Content-Type': 'text/plain'});
+     res.write("skriver...");
+     req.pipe(process.stdout);
+     res.end('piping done');
   } else {
      res.writeHead(404, {'Content-Type': 'text/plain'});
      res.end('not message here');
   }
-}).listen(80,'192.168.1.13');
+}).listen(80,'10.0.0.5');
 console.log("Listening");
 
